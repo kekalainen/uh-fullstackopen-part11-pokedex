1d8d9f20a837a1862c90b5c053f7b795beb05082
Kekalainen <git@kekalainen.me>
2023-12-22 00:31:56 +0200

    Add test for card page, refactor

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,7 +1,16 @@
+const baseUrl = 'http://localhost:5000'
+
 describe('Pokedex static server', function() {
   it('serves the front page properly', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit(baseUrl)
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('allows one to navigate from the main page to a card page', function() {
+    cy.visit(baseUrl)
+    cy.contains('bulbasaur').click()
+    cy.contains('ability')
+    cy.contains('overgrow')
   })
 })
